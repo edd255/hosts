059c639e902b9877ed1fec7728a46ffcf3e408f2
abaddon <abaddon@disroot.org>
2023-01-07 20:44:24 +0100

    feat: added Reddit alternatives

--- combiner.sh
+++ combiner.sh
@@ -18,3 +18,31 @@ sed -i 's/  //g' hosts
 sed -i 's/\t//g' hosts
 sed -i '/^[[:blank:]]*$/ d' hosts
 sort hosts | uniq | sponge hosts
+
+wget https://raw.githubusercontent.com/libreddit/libreddit-instances/master/instances.json
+jq ".instances | .[] | .url" instances.json >> temp
+sed -i 's/"//g' temp
+sed -i 's/null//g' temp
+sed -i 's/  //g' temp
+sed -i 's/\t//g' temp
+sed -i "s/https:\/\///g" temp
+sed -i "s/\///g" temp
+sed -i '/^[[:blank:]]*$/ d' temp
+sed -i 's/^/0\.0\.0\.0 /' temp
+cat temp >> hosts
+rm instances.json
+rm temp
+
+wget https://codeberg.org/teddit/teddit/raw/branch/main/instances.json
+jq ".[] | .url" instances.json >> temp
+sed -i 's/"//g' temp
+sed -i 's/null//g' temp
+sed -i 's/  //g' temp
+sed -i 's/\t//g' temp
+sed -i "s/https:\/\///g" temp
+sed -i "s/\///g" temp
+sed -i '/^[[:blank:]]*$/ d' temp
+sed -i 's/^/0\.0\.0\.0 /' temp
+cat temp >> hosts
+rm instances.json
+rm temp
